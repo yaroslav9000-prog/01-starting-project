f4e11958ca8e4ef2769e4f6aeddda3ed2fce93e3
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-07-27 15:07:00 +0300

    some commit

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -3,36 +3,47 @@ import { newTaskData, userTask } from "../Models/User";
 
 @Injectable({providedIn: 'root'})
 export class TasksService{
-    private tasks = [
-        {
-          id: 't1',
-          userId: 'u1',
-          title: 'Master Angular',
-          summary:
-            'Learn all the basic and advanced features of Angular & how to apply them.',
-          dueDate: '2025-12-31',
-        },
-        {
-          id: 't2',
-          userId: 'u3',
-          title: 'Build first prototype',
-          summary: 'Build a first prototype of the online shop website',
-          dueDate: '2024-05-31',
-        },
-        {
-          id: 't3',
-          userId: 'u3',
-          title: 'Prepare issue template',
-          summary:
-            'Prepare and describe an issue template which will help with project management',
-          dueDate: '2024-06-15',
-        },
-      ];
+    private tasks !: userTask[]; 
+    // [
+    //     {
+    //       id: 't1',
+    //       userId: 'u1',
+    //       title: 'Master Angular',
+    //       summary:
+    //         'Learn all the basic and advanced features of Angular & how to apply them.',
+    //       dueDate: '2025-12-31',
+    //     },
+    //     {
+    //       id: 't2',
+    //       userId: 'u3',
+    //       title: 'Build first prototype',
+    //       summary: 'Build a first prototype of the online shop website',
+    //       dueDate: '2024-05-31',
+    //     },
+    //     {
+    //       id: 't3',
+    //       userId: 'u3',
+    //       title: 'Prepare issue template',
+    //       summary:
+    //         'Prepare and describe an issue template which will help with project management',
+    //       dueDate: '2024-06-15',
+    //     },
+    //   ];
+      constructor(){
+        const tasks = localStorage.getItem('tasks');
+
+        if(tasks){
+            this.tasks = JSON.parse(tasks);
+        }else{
+            this.tasks = [];
+        }
+      }
       getSelectedUserTasks(userID: string){
         return this.tasks.filter((item:userTask)=>(item.userId === userID));
       }
       removeTask(id: string){
         this.tasks = this.tasks.filter((item: userTask)=> (item.id !== id))
+        this.saveTasks();
       }
       onAddTask(taskData: newTaskData, userID: string){
         this.tasks.unshift({
@@ -42,6 +53,9 @@ export class TasksService{
           summary: taskData.summary,
           dueDate: taskData.dueDate
         })
-    
+        this.saveTasks();
+      }
+      private saveTasks(){
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
       }
 }
